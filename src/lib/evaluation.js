4356dbc2df0f52e3fb0c111b5115c8c9621b830c
DESKTOP-CAGBJ2A\Duc Anh <anhhd.vnist@gmail.com>
2021-11-22 09:58:08 +0700

    fix import

--- src/lib/evaluation.js
+++ src/lib/evaluation.js
@@ -1,18 +1,5 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore, getDocs, collection } from "firebase/firestore";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAsqAL18hjTKzpYNPW3q6lSKaejYO1TuDc",
-  authDomain: "kibo-art-academy.firebaseapp.com",
-  projectId: "kibo-art-academy",
-  storageBucket: "kibo-art-academy.appspot.com",
-  messagingSenderId: "182717419459",
-  appId: "1:182717419459:web:c9090ca49ad66aa7c8b776"
-};
-
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-
+import {getDocs, collection } from "firebase/firestore";
+import {db} from "./firebase";
 
 export const updateAchievementsItem = async (id, score, maxScore) => {
     try {
