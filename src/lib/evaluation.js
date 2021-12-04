83f323dd801702db84084b9f5548d94092b57f5f
DESKTOP-CAGBJ2A\Duc Anh <anhhd.vnist@gmail.com>
2021-12-04 10:34:25 +0700

    evaluation

--- src/lib/evaluation.js
+++ src/lib/evaluation.js
@@ -1,28 +1,87 @@
-import {getDocs, collection, updateDoc, doc } from "firebase/firestore";
-import {db} from "./firebase";
+import {
+  getFirebaseItems,
+  getFirebaseItemWithCondition,
+  updateItemFireBase,
+} from "./firebase"
 
-export const updateAchievementsItem = async (id, score, maxScore) => {
-    try {
-      const data = updateDoc(
-        doc(db,"/Classes/97sxQMGJ5pQj80JnmodI/ClassLessons/yXhng4x1MPLSJj1D6z2w/Achievements/a6vNUFGxDdsalwT9szg2"),
-        {
-          score: score,
-          maxScore: maxScore,
-        }
-      );
-      return data;
-    } catch (err) {
-    }
+export const updateAchievementsItem = async (classID, LessonID, talents) => {
+  console.log("updateAchievementsItem", classID, LessonID, talents)
+  const updateItem = (item) => {
+    return new Promise((resovle, reject) => {
+      const newItem = { score: item.score }
+      updateItemFireBase(
+        newItem,
+        "Classes",
+        classID,
+        "ClassLessons",
+        LessonID,
+        "Achievements",
+        item.id
+      )
+        .then((isSuccess) => resovle(isSuccess))
+        .catch((error) => reject(false))
+    })
+  }
+  let isSuccess = false
+
+  isSuccess = await Promise.all(talents.map((item) => updateItem(item)))
+    .then((values) => values.every((item) => item === true))
+    .catch((isSuccess = false))
+  return isSuccess
 }
 
-export const getFirebaseItems = async (table) => {
-    try {
-      const result = await getDocs(collection(db, table));
-      result.forEach((doc) => {
-        console.log(doc.data());
-      });
-      return result;
-    } catch (err) {
-      console.log(err);
-    }
+export const getAllTalentsByClassUID = async (classId, lessonID) => {
+  let talents = []
+  const getAchivement = (lessonID) => {
+    return new Promise((resovle, reject) => {
+      getFirebaseItems(
+        "Classes",
+        classId,
+        "ClassLessons",
+        lessonID,
+        "Achievements"
+      )
+        .then((data) => resovle({ data: data, lessonID }))
+        .catch((error) => reject(error))
+    })
   }
+  try {
+    //Get all talents in the class
+    const Lessons = await getFirebaseItems("Classes", classId, "ClassLessons")
+    const data = await Promise.all(
+      Lessons.map((lesson) => getAchivement(lesson.id))
+    )
+    const index = data.findIndex((item) => item.lessonID === lessonID)
+    const LessonSelected = data[index].data
+
+    // Get talent information by talentId
+    for (let i = 0; i < LessonSelected.length; i++) {
+      const studentInfo = await getFirebaseItemWithCondition("Users", [
+        "userID",
+        "==",
+        LessonSelected[i].talentID,
+      ])
+
+      talents.push({
+        ...studentInfo,
+        ...LessonSelected[i],
+      })
+    }
+  } catch (err) {
+    console.log(err);
+  }
+  return talents
+}
+
+export const getClassesLesson = async (classId) => {
+  let lessons = []
+  try {
+    lessons = await getFirebaseItems("Classes", classId, "ClassLessons")
+    lessons = lessons.sort((a, b) => {
+      return b.date.seconds - a.date.seconds
+    })
+  } catch (error) {
+    console.log(error)
+  }
+  return lessons
+}
