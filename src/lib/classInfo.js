e2d5bcc059e614b07f154c71c4351e44d02b1a31
locckhl <locckhl123@gmail.com>
2022-01-14 10:55:11 +0700

    Fix add lesson

--- src/lib/classInfo.js
+++ src/lib/classInfo.js
@@ -1,42 +1,66 @@
-import { collection, doc, setDoc, getDocs } from "@firebase/firestore"
-import { db } from "./firebase"
-import { getFirebaseItemWithCondition } from "./firebase"
+import { collection, doc, setDoc } from "@firebase/firestore";
+import { db, getFirebaseItemWithCondition } from "./firebase";
 
 export const createLesson = async ({
   title,
   dateFormatted,
   classUID,
-  talentIds
+  talentIds,
 }) => {
   await setDoc(doc(collection(db, "Classes", classUID, "ClassLessons")), {
     name: title,
     date: dateFormatted,
   })
     .then(async (data) => {
-      const createdLesson = await getFirebaseItemWithCondition(`Classes/${classUID}/ClassLessons`, [
-        "name",
-        "==",
-        title,
-      ])
+      const createdLesson = await getFirebaseItemWithCondition(
+        `Classes/${classUID}/ClassLessons`,
+        ["name", "==", title]
+      );
 
-      await setDoc(doc(db, `Classes/${classUID}/ClassLessons/${createdLesson.id}/Achievements`, "mail"),{talentIds});
-      // await setDoc(
-      //   doc(collection(db, `Classes/${classUID}/ClassLessons/${createdLesson.id}/Achievements`)),
-      //   {
-      //     talentIDs: talentIds,
-      //   }
-      // )
-      console.log(talentIds,"talentIds");
-      await setDoc(
-        doc(collection(db, `Classes/${classUID}/ClassLessons/${createdLesson.id}/Attendances`)),
-        {
-          talentIDs: talentIds,
-        }
-      )
+      const setAchievements = talentIds.map(async (talentId) => {
+        const talent = await getFirebaseItemWithCondition("Users", [
+          "userID",
+          "==",
+          talentId,
+        ]);
 
-      // window.location.href = `/classDetail/${ classUID }`
+        await setDoc(
+          doc(
+            db,
+            `Classes/${classUID}/ClassLessons/${createdLesson.id}/Achievements`,
+            talent.email
+          ),
+          {
+            score: 0,
+            talentID: talentId,
+          }
+        );
+      });
+
+      Promise.all(setAchievements);
+
+      const setAttendances = talentIds.map(async (talentId) => {
+        const talent = await getFirebaseItemWithCondition("Users", [
+          "userID",
+          "==",
+          talentId,
+        ]);
+        await setDoc(
+          doc(
+            db,
+            `Classes/${classUID}/ClassLessons/${createdLesson.id}/Attendances`,
+            talent.email
+          ),
+          {
+            status: false,
+            talentID: talentId,
+          }
+        );
+      });
+
+      Promise.all(setAttendances);
     })
     .catch((err) => {
-      throw err
-    })
-}
+      throw err;
+    });
+};
